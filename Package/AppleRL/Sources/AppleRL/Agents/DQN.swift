e58191957f0d69351d0f4c1629fcc887e613319b
Alessandro Pavesi <pavesialessandro@live.it>
2021-12-14 10:51:52 +0100

    New timeinterval for background task

--- Package/AppleRL/Sources/AppleRL/Agents/DQN.swift
+++ Package/AppleRL/Sources/AppleRL/Agents/DQN.swift
@@ -99,11 +99,11 @@ open class DeepQNetwork {
         self.gamma = (parameters["gamma"] as? Double)!
         self.epochs = 10 //(parameters["epochs"] as? Float)!
         self.learningRate = (parameters["learning_rate"] as? Double)!
-        self.timeIntervalTrainingBackgroundMode = Double(30*60)
+        self.timeIntervalTrainingBackgroundMode = Double(2*60*60) // 2 ore
         if let val = parameters["timeIntervalBackgroundMode"] {
             self.timeIntervalBackgroundMode = val as! Double
         } else {
-            self.timeIntervalBackgroundMode = Double(10*60)
+            self.timeIntervalBackgroundMode = Double(10*60) // 10 minuti
         }
         defaultLogger.log("DQN Initialized")
         loadUpdatedModel()
