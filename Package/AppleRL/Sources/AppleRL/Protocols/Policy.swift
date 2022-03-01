2c883f79ce914fbc91c0e5aea7100ba5a0690cd4
Alessandro Pavesi <pavesialessandro@live.it>
2022-03-01 18:08:36 +0100

    New policies

--- Package/AppleRL/Sources/AppleRL/Protocols/Policy.swift
+++ Package/AppleRL/Sources/AppleRL/Protocols/Policy.swift
@@ -29,8 +29,10 @@ public class EpsilonGreedy: Policy {
     var step: Int = 0 // save this value?
     
     private func defineEpsilon() -> Double {
-        if self.step < 500 {
-            return 0.6
+        if self.step < 5500 {
+            return 0.7
+        } else if self.step < 10500 {
+            return 0.5
         } else {
             return 0.3
         }
@@ -50,6 +52,63 @@ public class EpsilonGreedy: Policy {
             let stateValue = MLFeatureValue(multiArray: state)
             // predict value from model
             defaultLogger.log("State Value \(convertToArray(from: state))")
+            let stateTarget = model.predictForAppleRL(stateValue) // try? model.prediction(from: ) //
+            print(stateTarget)
+            
+//            defaultLogger.log("Model Choice \(convertToArray(from: stateTarget!.actions).argmax()!)")
+//            defaultLogger.log("Model List \(convertToArray(from: stateTarget!.actions))")
+            return convertToArray(from: stateTarget!.actions).argmax()!
+        }
+    }
+}
+
+public class RandomPolicy: Policy {
+    public var id: Int = 0
+    
+    public init() {}
+    
+    public var description: String = "Random"
+    
+    public func exec(model: MLModel, state: MLMultiArray) -> Int {
+        defaultLogger.log("\(self.description)")
+        return Int.random(in: 0...3)
+    }
+}
+
+public class EpsilonGreedy3: Policy { // TODO Roba non accessibile perchè  internal: .actions
+    public var id: Int = 0
+    
+    public init() {}
+    
+    public var description: String = "EpsilonGreedy"
+    
+    let greedy: Bool = false
+    var step: Int = 0 // save this value?
+    
+    private func defineEpsilon() -> Double {
+        if self.step < 15000 {
+            return 0.7
+        } else if self.step < 50000 {
+            return 0.5
+        } else {
+            return 0.3
+        }
+    }
+    
+    public func exec(model: MLModel, state: MLMultiArray) -> Int {
+        print("\(self.description)")
+        let currentEpsilon = defineEpsilon()
+        self.step += 1
+        if !greedy && Double.random(in: 0..<1) < currentEpsilon {
+            // epsilon choice
+            let choice = Int.random(in: 0..<3)
+            print("Epsilon Choice \(choice)")
+            return choice
+        }
+        else {
+            let stateValue = MLFeatureValue(multiArray: state)
+            // predict value from model
+            print("State Value \(convertToArray(from: state))")
             let stateTarget = model.predictForAppleRL(stateValue) // try? model.prediction(from: ) //
             print(stateTarget)
             
