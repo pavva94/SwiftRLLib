a2e116db4f39199297fa479581ad8d8897e3b979
Alessandro Pavesi <pavesialessandro@live.it>
2022-03-04 10:14:33 +0100

    New Environment2D app

---
+++ ExampleApps/Environment2D/Environment2D/AppSensors.swift
@@ -0,0 +1,66 @@
+//
+//  AppSensors.swift
+//  Environment2D
+//
+//  Created by Alessandro Pavesi on 02/03/22.
+//
+
+import Foundation
+import AppleRL
+
+open class MatrixSens: ObservableData {
+    
+    init() {
+        super.init(name: "matrix", stateSize: 10)
+    }
+    var sensStep: Int = 0
+    let maxStep: Int = 250
+    let originalWorld: [Double]  = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var world: [Double] = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    open override func read(_ state: [Double] = []) -> [Double]
+    {
+        if sensStep < maxStep {
+            self.sensStep += 1
+            return self.world
+        } else {
+            reset()
+            return []
+        }
+    }
+    
+    open override func preprocessing(value: Any) -> [Double] {
+        return [value as! Double]
+    }
+    
+    public func moveLeft() {
+        let posIndex = self.world.firstIndex(of: 1.0)!
+        if posIndex <= self.world.count-1 && posIndex > 0 {
+            self.world[posIndex] = 0
+            self.world[posIndex-1] = 1
+        }
+        else {
+            print("Agent limited by barrier")
+        }
+        
+    }
+    
+    public func moveRight() {
+        let posIndex = self.world.firstIndex(of: 1.0)!
+        if posIndex < self.world.count-1 && posIndex >= 0 {
+            self.world[posIndex] = 0
+            self.world[posIndex+1] = 1
+        }
+        else {
+            print("Agent limited by barrier")
+        }
+        
+    }
+    
+    func reset() {
+        print("Reset \(self.sensStep)")
+        self.world = self.originalWorld
+        self.sensStep = 0
+    }
+}
+
